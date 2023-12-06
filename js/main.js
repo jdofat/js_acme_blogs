af5e51692726e38db91e01ccf80fc59802755ed1
Jade Dofat <j_dofat@mail.fhsu.edu>
2023-12-06 18:12:22 -0500

    push4

--- js/main.js
+++ js/main.js
@@ -7,11 +7,28 @@ function createElemWithText (stringOne="p", textContent="hi", firstClassName) {
     };
     return elText;
 }
-createElemWithText();
+
+/* 2. createSelectOptions */
+function createSelectOptions (userData) {
+    if (!userData) {
+        return ;
+    }
+    let myOptionsArray = [];
+    for (let i = 0; i < userData.length; i++) {
+        let opt = document.createElement("option");
+        opt.value = userData[i].id;
+        opt.textContent = userData[i].name;
+        myOptionsArray.push(opt); 
+        }
+    return myOptionsArray;
+}
 
 /**/
 
-
+/**/
+/**/
+/**/
+/**/
 
 
 
